3f8ee528362732ab9eafc20cfc0046dbc276ceb8
Victor Pinedo <victor.pinedo@sailpoint.com>
2023-05-02 15:01:22 -0500

    21. The View Children Decorator and Query List In Detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import {COURSES} from '../db-data';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { COURSES } from '../db-data';
 import { Course } from './model/course';
 import { CourseCardComponent } from './course-card/course-card.component';
 
@@ -12,18 +12,27 @@ export class AppComponent implements AfterViewInit {
 
   courses = COURSES;
 
-  @ViewChild("cardRef1", { read: ElementRef })
-  card: CourseCardComponent;
-
-  @ViewChild("courseImage")
-  courseImage: ElementRef;
+  @ViewChildren(CourseCardComponent, { read: ElementRef })
+  cards: QueryList<CourseCardComponent>;
 
   constructor() {}
   
   ngAfterViewInit(): void {
-    console.log("courseImage", this.courseImage);
+    // this.cards.changes.subscribe(console.log);
+    console.log(this.cards);
   }
   
+  onCouresEdited() {
+    this.courses.push({
+      id: 2,
+      description: "RxJs In Practice Course",
+      iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/rxjs-in-practice-course.png',
+      longDescription: "Understand the RxJs Observable pattern, learn the RxJs Operators via practical examples",
+      category: 'BEGINNER',
+      lessonsCount: 10
+    });
+  }
+
   onCourseSelected(course: Course) {
   }
 
