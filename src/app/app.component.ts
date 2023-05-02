46e28414ba69dce39dd46b3f4f7e9c3ca942d868
Victor Pinedo <victor.pinedo@sailpoint.com>
2023-05-02 14:27:57 -0500

    ViewChild decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import {COURSES} from '../db-data';
 import { Course } from './model/course';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
   courses = COURSES;
+
+  @ViewChild("cardRef1", { read: ElementRef })
+  card: CourseCardComponent;
+
+  @ViewChild("courseImage")
+  courseImage: ElementRef;
+
+  constructor() {}
+  
+  ngAfterViewInit(): void {
+    console.log("courseImage", this.courseImage);
+  }
   
   onCourseSelected(course: Course) {
-    console.log('AppComponent - click event bubbled...', course);
   }
 
 }
